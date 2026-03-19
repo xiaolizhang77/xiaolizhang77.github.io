d1174b9dd96112207eff12f924c7babb1a98415c
xiaolizhang77 <115877992+xiaolizhang77@users.noreply.github.com>
2026-03-19 03:51:44 +0000

    Deploying to gh-pages from @ xiaolizhang77/xiaolizhang77.github.io@06dfd5c1dcb184e33f98a46e20668e4cb6acf514 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,7 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%7A%68%61%6E%67%71%6C%32%30%32%33@%6D%61%69%6C.%7A%67%63%6C%61%62.%65%64%75.%63%6E", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -539,14 +539,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=MKR62vEAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
