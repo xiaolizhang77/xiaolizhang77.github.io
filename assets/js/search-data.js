42d9af9510b0853bc251e25e9066ba681c3dbf80
xiaolizhang77 <115877992+xiaolizhang77@users.noreply.github.com>
2026-06-04 13:03:21 +0000

    Deploying to gh-pages from @ xiaolizhang77/xiaolizhang77.github.io@903dc0974b1e5b88a15c5032a2b950222c640203 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Curriculum Vitae of Qili Zhang.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -35,7 +35,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/cv.pdf", "_blank");
         },
       },{
         id: 'social-email',
