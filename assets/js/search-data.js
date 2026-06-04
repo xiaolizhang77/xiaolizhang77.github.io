be6af3b873e8cd4982aa76f312d4fce59bee41b8
xiaolizhang77 <115877992+xiaolizhang77@users.noreply.github.com>
2026-06-04 12:54:29 +0000

    Deploying to gh-pages from @ xiaolizhang77/xiaolizhang77.github.io@60a0f62b9f3e7a5fe682db1fcd3a86600946c320 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,21 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{
+        },{
         id: 'social-cv',
         title: 'CV',
         section: 'Socials',
